89f7dbd7997521ea2f1b70ee288ddc48d8df402c
Melvin <morel.melvin@gmail.com>
2022-10-20 11:51:18 -0400

    new

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,23 +1,12 @@
-
-def totalbill_and_sales_amount(amount):
-    sales_amt = (amount * 0.1)
-    total_amt = sales_amt + amount
-    return round(total_amt,2)
-
-
-
+# returns total bill with tax added
+def sales_tax(amount):
+    return (amount * 0.10)
+   
 # This function will return the tip amount. The function has NOT added the tip to the bill yet.
 def tip_selection(total_bill):
-    tip = float(input(f' How much would you like to tip  15% 20% 25% ? or please enter custom amount ' ))
-    return  (tip /100) * total_bill 
-   
-
-
-# this functino will split the bill
-def number_of_people(people,total_bill):
-    return  round(total_bill /people, 2)
-    
-    
+    tip = float(input(f'How much would you like to tip  15% 20% 25% ? or please enter custom amount ' ))
+    tip_total_amount = total_bill * (0.01*tip)
+    return tip_total_amount
 
 while True:
 # Here I am going to ask the user how much the total is.
@@ -28,12 +17,11 @@ while True:
 
 
 # calculate sales tax and round the amount 2 decimal points.
-    total_bill = totalbill_and_sales_amount(amount)
-    tip_amount_only = tip_selection(total_bill)
-    tip_and_total_bill = total_bill + tip_amount_only
-    split_amount = number_of_people(guest,tip_and_total_bill)
-
-    print(split_amount)
+    sales_tax = sales_tax(amount)
+    tip_amount_only = tip_selection(amount)
+    final_bill = (amount + tip_amount_only + sales_tax) / guest
+    
+    print(round(final_bill,2))
     user_input = input(f'Do you wish to continue ? Enter Yes or No ').strip().upper()
     while user_input not in ['YES','NO']:
         print('You must enter Yes or No ')
