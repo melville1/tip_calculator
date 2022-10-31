268bafd58d38dc8bcd8eb1f02a694be1e95a7c30
Melvin <morel.melvin@gmail.com>
2022-10-31 18:56:24 -0400

    new

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # tip_calculator
+
+## Description
+
+- This calculator is intended to be used to determined the cost amount at a restaurant. 
+- The calculator is equipped to handle any amount of guest.
+- The calculator has pre-determined tip amounts as well as a custom option. 
+- Sales tax is calculated in the program and can be changed by identifying the sales tax variable. (for those living in different states)
+- The calculator will continue to run until a calculation is executed. 
+- The calculator is not case sensitive and spaces will not interrupt the code.
+
+## Installation
+
+- You will need Python 3.10.x or above; You can download it [here](https://www.python.org/)
+- Clone the repo using HTTPS:
+```
+git clone https://github.com/melville1/tip_calculator.git
+```
+- Clone the repo using SSH:
+```
+git clone git@github.com:melville1/tip_calculator.git
+```
+
+## Usage
+
+The calculator will first ask for the dollar amount. The dollar amount has to be a float. The next step involves the number of parties that the amount will be divided. 
+
+Next, functions are called then stored into variables (this happens simultaneously)
+the calculator is prepared to handle any traditional error that may occur during a tranaction. 
+
--- tip_calculator.py
+++ tip_calculator.py
@@ -4,28 +4,33 @@ def sales_tax(amount):
    
 # This function will return the tip amount. The function has NOT added the tip to the bill yet.
 def tip_selection(total_bill):
-    tip = float(input(f'How much would you like to tip  15% 20% 25% ? or please enter custom amount ' ))
-    tip_total_amount = total_bill * (0.01*tip)
+    try: 
+        tip = float(input(f'How much would you like to tip  15% 20% 25% ? or please enter custom amount ' ))
+        tip_total_amount = total_bill * (0.01*tip)
+    except:
+        print('Wrong value please enter a float')
     return tip_total_amount
 
-while True:
-# Here I am going to ask the user how much the total is.
-    amount = float(input(f'What is your total bill ? '))
-# I will be asking how many people are paying.
-    guest = int(input(f'How many people are paying ? '))
-# Then I will be asking how much they want to tip 
+user_input = "YES"
+while user_input == "YES":
+    try: 
+    # Here I am going to ask the user how much the total is.
+        amount = float(input(f'What is your total bill ? '))
+    # I will be asking how many people are paying.
+        guest = int(input(f'How many people are paying ? '))
+    # Then I will be asking how much they want to tip 
 
 
-# calculate sales tax and round the amount 2 decimal points.
-    sales_tax = sales_tax(amount)
-    tip_amount_only = tip_selection(amount)
-    final_bill = (amount + tip_amount_only + sales_tax) / guest
-    
-    print(round(final_bill,2))
-    user_input = input(f'Do you wish to continue ? Enter Yes or No ').strip().upper()
-    while user_input not in ['YES','NO']:
-        print('You must enter Yes or No ')
+    # calculate sales tax and round the amount 2 decimal points.
+        salesTax = sales_tax(amount)
+        tip_amount_only = tip_selection(amount)
+        final_bill = (amount + tip_amount_only + salesTax) / guest
+        
+        print(round(final_bill,2))
         user_input = input(f'Do you wish to continue ? Enter Yes or No ').strip().upper()
-    if user_input == 'NO':
-        break 
+        while user_input not in ['YES','NO']:
+            print('You must enter Yes or No ')
+            user_input = input(f'Do you wish to continue ? Enter Yes or No ').strip().upper()
+    except ValueError:
+        print("Please enter a valid number")
 print('God Bless')
